81853b0111907a2995e37d3fd13a5a3a76a520c4
minji1160 <97896080+minji1160@users.noreply.github.com>
2024-08-21 17:25:49 +0900

    Update 프로세스.cpp
    답변 추가 및 분석

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -26,3 +26,28 @@ int solution(vector<int> priorities, int location) {
     }
     return answer;
 }
+
+/*
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    int max = *max_element(priorities.begin(), priorities.end()); // max priority값 저장
+    while (true)
+    {
+        for (int i = 0; i < priorities.size(); ++i)
+        {
+            if (priorities[i] == max)//실행순서면
+            {
+                ++answer;//실행된 프로세스 수 +1
+
+                if (i == location)//idx가 location과 동일한지 확인
+                    return answer;
+
+                priorities[i] = 0; // 실행된 프로세스의 priority 제거
+                max = *max_element(priorities.begin(), priorities.end()); //max값 초기화
+            }
+            //실행순서가 아닐 경우 그냥 넘어감. 따로 queue를 수정하지 않아도 round robin 처럼 pointer가 돈다고 생각하면 됨.
+            //pointer가 돌 경우 priority가 0인 것은 무시하는 효과를 가짐
+        }
+    }
+}
+*/
