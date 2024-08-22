8dfc067709e8b4791f4c4d217ccf423482c4f08e
minji1160 <97896080+minji1160@users.noreply.github.com>
2024-08-22 14:53:00 +0900

    Update 다리를 지나는 트럭.cpp
    주석추가

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 0;
+    int answer = 0; //answer을 시간으로 보고 계산
     queue<int> bridge;
     reverse(truck_weights.begin(), truck_weights.end());
     int cur_weight = 0;
@@ -16,6 +16,8 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
             cur_weight -= bridge.front();
             bridge.pop();
         }
+        // 맨 앞에 있던 차량이 빠지는 것과 이후 추가진입을 하는 것은 동시에 일어날 수 있으므로 별개의 if
+        
         if(cur_weight + truck_weights.back() > weight)
             bridge.push(0);
         else{
@@ -24,6 +26,7 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
             truck_weights.pop_back();
         }
     }
+    // 마지막 차량 진입 이후 while 종료이므로 bridge_legnth 만큼의 시간 추가 필요
     answer += bridge_length;
     return answer;
 }
