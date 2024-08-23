bcd15fcdeb8de731c110d0c3545d470ad90c5145
minji1160 <97896080+minji1160@users.noreply.github.com>
2024-08-23 17:44:03 +0900

    [level 1] Title: 모의고사, Time: 0.05 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -20,26 +20,3 @@ vector<int> solution(vector<int> answers) {
     if(score3 == max) answer.push_back(3);
     return answer;
 }
-
-/*
-#include <algorithm>
-
-vector<int> one = {1,2,3,4,5};
-vector<int> two = {2,1,2,3,2,4,2,5};
-vector<int> thr = {3,3,1,1,2,2,4,4,5,5};
-
-vector<int> solution(vector<int> answers) {
-    vector<int> answer;
-    vector<int> score(3);
-    for(int i=0; i<answers.size(); i++) {
-        if(answers[i] == one[i%one.size()]) score[0]++;
-        if(answers[i] == two[i%two.size()]) score[1]++;
-        if(answers[i] == thr[i%thr.size()]) score[2]++;
-    }
-    int max_score = *max_element(score.begin(),score.end());
-    for(int i = 0; i< 3; i++) {
-        if(they[i] == they_max) answer.push_back(i+1);
-    }
-    return answer;
-}
-*/
