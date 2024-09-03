79af348920b7923a357134dc5161fe2e635df26f
minji1160 <97896080+minji1160@users.noreply.github.com>
2024-09-03 16:02:51 +0900

    Update 피로도.cpp

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -18,5 +18,6 @@ int solution(int k, vector<vector<int>> dungeons) {
             answer = max(answer, cnt);
         }
     }while(next_permutation(dungeons.begin(), dungeons.end()));
+    // next_permutation : vector든 string이든 상관없이 item들의 집합으로 순열의 모든 가능성을 돌게 해주는 함수..?
     return answer;
 }
