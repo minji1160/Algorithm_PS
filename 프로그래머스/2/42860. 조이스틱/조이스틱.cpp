139538bd2bd136d906e1040fc9002e68d7867209
minji1160 <97896080+minji1160@users.noreply.github.com>
2024-09-05 15:50:39 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,12 +14,16 @@ int solution(string name) {
         
         //2. 양 옆으로의 이동의 최소값
         int idx2 = i + 1;
-        while(idx2 < name.size()){
-            if(name[idx2] == 'A') idx2++;
-            else break;
+        while(idx2 < name.size() && name[idx2] == 'A'){
+            idx2++;
+            //현재 위치에서 다음 위치 중 A가 아닌 위치까지 이동
         }
-        int L2 = name.size() - idx2;
+        int L2 = name.size() - idx2; //다음 위치에서 끝까지의 길이
         move = min(move, i + L2 + min(i, L2));
+        //현 위치를 기준으로 계속 오른쪽으로 이동거리,
+        //or 오(시작~i)->왼(i~시작)->왼(끝~idx2) 이동거리,
+        //or 왼(끝~idx2)->오(idx2~끝)->오(시작~i)
+        //중 가장 작은 것으로 선택 
     }
     answer += move;
     return answer;
