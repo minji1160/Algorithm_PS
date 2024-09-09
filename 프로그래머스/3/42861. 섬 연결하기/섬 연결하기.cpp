f00ac0b089b3507d6021c71739fd4d3de2bfb10c
minji1160 <97896080+minji1160@users.noreply.github.com>
2024-09-09 22:35:03 +0900

    Update 섬 연결하기.cpp
    Kruskal MST code 및 주석 추가

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -44,3 +44,41 @@ int solution(int n, vector<vector<int>> costs) {
     //if(count != n) answer = 0;
     return answer;
 }
+
+/*
+int d[101]; //출발 노드를 저장할 배열
+
+int getParent(int node){ // 입력으로 들어온 노드와 이어져 있는 노드 중 제일 처음에 있는 노드 반환
+    if(node == d[node]){
+        return node;
+    }
+    else return d[node] = getParent(d[node]);
+}
+
+bool compare(vector<int> a, vector<int> b){
+    return a[2] < b[2];
+}
+
+int solution(int n, vector<vector<int>> costs) {
+
+    int answer = 0;
+    for(int i =0; i<n; i++){
+        d[i] = i; // 아무것도 연결하지 않은 상태에서, 각 노드의 출발 노드는 자신이므로 i의 값을 넣어 저장
+    }
+    sort(costs.begin(), costs.end(), compare); //cost를 기준으로 정렬 
+    for(int i=0; i<costs.size(); i++){
+        int start = getParent(costs[i][0]);
+        int end = getParent(costs[i][1]);
+        int cost = costs[i][2];
+
+        if(start != end){ // start와 end가 같으면 cycle이 생기는 것이기 때문에 해당 edge는 넘김
+            d[end] = start;
+
+            answer += cost;
+        }
+    }
+
+
+    return answer;
+}
+*/
