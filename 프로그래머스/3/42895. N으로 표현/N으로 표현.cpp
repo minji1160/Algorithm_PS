d36c0006293c1ac8ad405c6a0ae7bf185aeaa64b
minji1160 <97896080+minji1160@users.noreply.github.com>
2024-09-10 16:20:43 +0900

    Update N으로 표현.cpp
    주석추가

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -10,6 +10,12 @@ int solution(int N, int number) {
         num = 10*num + N;
         nums[i].insert(num);
     }
+    /*
+    set 사용 이유는 수가 겹치면 추가하지 않도록 하기 위함.
+    nums의 인덱스는 N의 사용 횟수를 기준으로 둔 것.
+    아래 4중 for문을 통해 이전 배열에서 숫자를 하나씩 가져와 새로운 수를 조합하여 nums에 추가
+    모든 가능한 수를 다 만들어 둔 뒤에 탐색을 통해 숫자가 있는지 확인
+    */
     for(int i = 1; i < 8; i++){
         for(int j = 0; j < i; j++){
             for(int a : nums[j]){
