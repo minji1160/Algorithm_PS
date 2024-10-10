11162e905980d6bcbd94b0b6c382452fd7cce834
minji1160 <97896080+minji1160@users.noreply.github.com>
2024-10-10 14:31:54 +0900

    Update 소수 구하기.cc
    코드 추가

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -26,3 +26,24 @@ int main() {
 	}
 	return 0;
 }
+
+
+//************[O(N^(1/2)) 시간 풀이. 이거도 통과]*****************
+// bool isPrime(int n) {
+// 	for (int i = 2; i <= sqrt(n); i++) {
+// 		if (n % i == 0) return false;
+// 	}
+// 	return true;
+// }
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+// 	int M, N;
+// 	cin >> M >> N;
+//     M = (M == 1) ? 2 : M;
+// 	for (int i = M; i <= N; i++) {
+// 		if (isPrime(i)) cout << i << "\n";
+// 	}
+// 	return 0;
+// }
