9bab755a14144c6ab4e73abc8fe2e4f3891e1886
minji1160 <97896080+minji1160@users.noreply.github.com>
2024-10-16 11:11:24 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int tiles[1100];
+	tiles[1] = 1;
+	tiles[2] = 3;
+	int n;
+	cin >> n;
+	for (int i = 3; i <= n; i++) {
+		tiles[i] = tiles[i - 1] + tiles[i-2]*2;
+		if (tiles[i] >= 10007) tiles[i] %= 10007;
+	}
+	cout << tiles[n];
+	return 0;
+}
