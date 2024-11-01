5699ec0c149685f7276134d182485849991b0bb7
minji1160 <97896080+minji1160@users.noreply.github.com>
2024-11-01 18:50:48 +0900

    Update 가장 긴 증가하는 부분 수열 4.cc
    주석 추가

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -5,9 +5,8 @@
 using namespace std;
 
 int main() {
-	int n, A;
+	int n;
 	int arr[1000] = { 0, };
-	vector<int> dp_arr[1000];
 	int dp[1000] = { 0, };
 	stack<int> seq;
 	int max_idx = 0;
@@ -39,3 +38,36 @@ int main() {
 	}
 	return 0;
 }
+
+/*int main() {
+	int n;
+	int arr[1000] = { 0, };
+	vector<int> dp_arr[1000];
+	int max_idx = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	vector<int>dp;
+	for (int i = 0; i < n; i++) {
+		dp_arr[i] = dp;
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j]) {
+				if (dp_arr[i].size() < dp_arr[j].size())
+					dp_arr[i] = dp_arr[j];
+			}
+		}
+		dp_arr[i].push_back(arr[i]);
+		dp.clear();
+	}
+	for (int i = 0; i < n; i++) {
+		if (dp_arr[i].size() > dp_arr[max_idx].size())
+			max_idx = i;
+	}
+	cout << dp_arr[max_idx].size() << "\n";
+	for (auto a : dp_arr[max_idx])
+		cout << a << " ";
+	return 0;
+}*/
+
+/* 주석 처리한 코드가 첫 시도. 답은 맞는데 시간이나 메모리 측면에서 위의 코드보다 비효율적. 메모리 면에서는 거의 차이가 없는데 시간이 더 오래 걸린다. 벡터를 너무 많이 써서 그런듯..*/
