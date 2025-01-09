18a8b54190bf8d295e2d2f3f5583646ca2a917b1
minji1160 <97896080+minji1160@users.noreply.github.com>
2025-01-09 14:44:21 +0900

    [Silver V] Title: 집합, Time: 1020 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+
+set<int> numGroup;
+int M, arg;
+string cmd;
+int main() {
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    cin >> M;
+	while (M--) {
+		cin >> cmd;
+		if (cmd == "add") {
+			cin >> arg;
+			numGroup.insert(arg);
+		}
+		else if (cmd == "remove") {
+			cin >> arg;
+			if (numGroup.find(arg) == numGroup.end()) continue;
+			numGroup.erase(arg);
+		}
+		else if (cmd == "check") {
+			cin >> arg;
+			if (numGroup.find(arg) == numGroup.end()) cout << 0 << "\n";
+			else cout << 1 << "\n";
+		}
+		else if (cmd == "toggle") {
+			cin >> arg;
+			if (numGroup.find(arg) == numGroup.end()) numGroup.insert(arg);
+			else numGroup.erase(arg);
+		}
+		else if (cmd == "all") {
+			numGroup = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+		}
+		else {
+			numGroup.clear();
+		}
+
+	}
+	return 0;
+}
