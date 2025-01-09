11e255424e258e649c38c47164df7b0c023045ce
minji1160 <97896080+minji1160@users.noreply.github.com>
2025-01-09 14:45:21 +0900

    [Silver V] Title: 집합, Time: 688 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -33,8 +33,10 @@ int main() {
 			else numGroup.erase(arg);
 		}
 		else if (cmd == "all") {
-			numGroup = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-		}
+			for (int i = 0; i < 20; i++) {
+				numGroup.insert(i + 1);
+			}
+        }
 		else {
 			numGroup.clear();
 		}
