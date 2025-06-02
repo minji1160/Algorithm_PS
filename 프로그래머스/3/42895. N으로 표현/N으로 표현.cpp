65372d6a31c8217120f1e496fd0fa947db4a6c8f
김민지(MINJI KIM) <97896080+minji1160@users.noreply.github.com>
2025-06-02 16:06:25 +0900

    [level 3] Title: N으로 표현, Time: 1.95 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <vector>
 #include <unordered_set>
 using namespace std;
@@ -5,25 +6,21 @@ using namespace std;
 int solution(int N, int number) {
     int answer = -1;
     unordered_set<int> nums[8];
-    int num = 0;
+    int a = N;
     for(int i = 0; i < 8; i++){
-        num = 10*num + N;
-        nums[i].insert(num);
+        nums[i].insert(a);
+        a = a*10 + N;
     }
-    /*
-    set 사용 이유는 수가 겹치면 추가하지 않도록 하기 위함.
-    nums의 인덱스는 N의 사용 횟수를 기준으로 둔 것.
-    아래 4중 for문을 통해 이전 배열에서 숫자를 하나씩 가져와 새로운 수를 조합하여 nums에 추가
-    모든 가능한 수를 다 만들어 둔 뒤에 탐색을 통해 숫자가 있는지 확인
-    */
     for(int i = 1; i < 8; i++){
         for(int j = 0; j < i; j++){
-            for(int a : nums[j]){
-                for(int b : nums[i-j-1]){
-                    nums[i].insert(a*b);
-                    nums[i].insert(a+b);
-                    if(a>b) nums[i].insert(a-b);
-                    if(a/b > 0) nums[i].insert(a/b);
+            for(int a : nums[i-j-1]){
+                for(int b : nums[j]){
+                    nums[i].insert(a + b);
+                    if(a > b)
+                        nums[i].insert(a - b);
+                    nums[i].insert(a * b);
+                    if(b != 0)
+                        nums[i].insert(a / b);
                 }
             }
         }
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.24 MB, 시간: 2.35 ms
+메모리: 4.16 MB, 시간: 1.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 10일 16:02:31
+2025년 06월 02일 16:06:22
 
 ### 문제 설명
 
