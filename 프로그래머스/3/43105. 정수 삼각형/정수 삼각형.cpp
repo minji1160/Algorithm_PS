f4010b14829b67045d5db609fdf8c9817ea4db4a
김민지(MINJI KIM) <97896080+minji1160@users.noreply.github.com>
2025-06-02 17:25:53 +0900

    [level 3] Title: 정수 삼각형, Time: 0.93 ms, Memory: 8.95 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -1,22 +1,24 @@
-#include <vector>
+#include <string>
 #include <algorithm>
-#include <iostream>
+#include <vector>
 
 using namespace std;
 
-void max_way(vector<vector<int>> &tri, int cur_depth, int depth){
-    if(cur_depth != depth)
-       max_way(tri, cur_depth+1, depth);
-    
-    for(int i = 0; i < tri[cur_depth].size(); i++){
-           tri[cur_depth][i] += max(tri[cur_depth+1][i], tri[cur_depth+1][i+1]);
-           //cout << "tri " << cur_depth << " " << i << " update " << tri[cur_depth][i] << endl;
-    }  
-}
-
 int solution(vector<vector<int>> triangle) {
     int answer = 0;
-    max_way(triangle, 0, triangle.size()-2);
-    answer = triangle[0][0];
+    int tri[500][500]= {0, };
+    int height = triangle.size();
+    for(int i = 0; i < triangle[height-1].size(); i++){
+        tri[height-1][i] = triangle[height-1][i];
+    }
+    for(int i = height-2; i >= 0; i--){
+        for(int j = 0; j < triangle[i].size(); j++){
+            tri[i][j] = max(tri[i+1][j], tri[i+1][j+1]) + triangle[i][j];
+        }
+    }
+    answer = tri[0][0];
     return answer;
 }
+
+// 밑에서 부터 큰 수를 선택해서 올라감.
+// tri[500][500]
